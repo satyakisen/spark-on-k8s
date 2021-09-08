9e10440d9bd78c0e8e1c2f3298dbb85222565b5d
satyakisen <sensatyaki7@gmail.com>
2021-09-08 11:07:26 +0530

    Adding spark-rest-api

---
+++ spark-rest-api/src/main/scala/org/ssen/GreetingResource.scala
@@ -0,0 +1,12 @@
+package org.ssen
+
+import javax.ws.rs.{GET, Path, Produces}
+import javax.ws.rs.core.MediaType
+
+@Path("/hello")
+class GreetingResource {
+
+    @GET
+    @Produces(Array[String](MediaType.TEXT_PLAIN))
+    def hello() = "Hello RESTEasy"
+}
---
+++ spark-rest-api/src/test/scala/org/ssen/NativeGreetingResourceIT.scala
@@ -0,0 +1,6 @@
+package org.ssen
+
+import io.quarkus.test.junit.NativeImageTest
+
+@NativeImageTest
+class NativeGreetingResourceIT extends GreetingResourceTest
