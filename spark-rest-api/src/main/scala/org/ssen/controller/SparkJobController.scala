c1e5368c50032c03395b03be2c251b0e42cb3a3e
satyakisen <sensatyaki7@gmail.com>
2021-09-11 00:09:04 +0530

    Adding the ApplicationConfig and Changes to SparkJobController.scala

--- spark-rest-api/src/main/scala/org/ssen/controller/SparkJobController.scala
+++ spark-rest-api/src/main/scala/org/ssen/controller/SparkJobController.scala
@@ -4,18 +4,19 @@ import org.ssen.service.SparkService
 
 import javax.inject.Inject
 import javax.ws.rs.core.MediaType
-import javax.ws.rs.{GET, Path, Produces}
+import javax.ws.rs.{GET, Path, PathParam, Produces}
 
-
-@Path("/hello")
+@Path("")
 class SparkJobController {
 
     @Inject
-    val sparkService = SparkService()
+    val sparkService: SparkService = SparkService()
 
     @GET
     @Produces(Array[String](MediaType.APPLICATION_JSON))
-    def hello() = sparkService.getPodByName("Hello")
+    @Path("/{name}")
+    def hello(@PathParam("name") name: String) : String = sparkService.getPodByName(name)
+
 
 
 }
